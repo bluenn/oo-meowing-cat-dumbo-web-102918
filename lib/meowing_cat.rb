18b3c2bd7699b283c2c247bf9aa93c93ae478807
a lan <bluenowte97@gmail.com>
2018-11-12 02:35:14 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,6 +2,6 @@
 class Cat
   attr_accessor :name
   def meow
-    puts “meow!” # Instance Method, a particular baby is able to cry
+    puts "meow!" # Instance Method, a particular baby is able to cry
     end
 end
